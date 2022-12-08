cfafbbb9a8a8ca1ea924eea3e3dfeb2badecd805
AllRaos <rusl.chaban@gmail.com>
2022-12-08 23:14:35 +0200

    zagryz

--- test/javascript/index.js
+++ test/javascript/index.js
@@ -1,1 +1,28 @@
-console.log("95782381910");
+function myfirstfunk(){
+    const ovoci=["Pomidor", "Abrukos", "Ceba"];
+    let kilk=ovoci.length;
+    
+    let b="<ul>";
+    for (let a=0;a<kilk;a++) {
+        b+="<li>"+ovoci[a]+"</li>";
+       
+            
+        
+    }
+    b+="</ul>";
+    document.getElementById("txt").innerHTML=b;
+}
+function myarray(){
+    let m=[];
+    let g=[];
+    let result;
+    for (let i=0;i<10;i++) {
+        
+        m[i]=i*2;
+        if (i%2===0) {
+            g[i]=i;
+        }
+        
+    }
+    document.getElementById("txt").innerHTML=m+" "+g;
+}
